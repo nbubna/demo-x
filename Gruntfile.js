ba73c54919d3686e5998216bf61f1d6ff4c4379f
nbubna <nathan@esha.com>
2014-12-02 17:03:08 -0800

    move/delete domx docs stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*global module:false*/
 module.exports = function(grunt) {
 
@@ -10,136 +12,91 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-
+    pkg: grunt.file.readJSON('bower.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
     clean: {
-      dependencies: {
-        src: ["bower_components",
-              "node_modules"]
-      },
-      maps: {
-        src: ["script/*.map",
-              "style/*.map"]
-      },
-      temp: {
-        src: ['temp']
-      },
-      artifacts: {
-        src: ["<%= uglify.js.dest %>",
-              "<%= csswring.css.dest %>",
-              "index.html",
-              "404.html",
-              "iframe.html"]
-      }
+      files: ['dist']
     },
-    copy: {
+    frame: {
       dist: {
-        src: 'bower_components/domx/dist/domx.min.js.gz',
-        dest: 'domx.min.js.gz'
-      }
-    },
-    bower_concat: {
-      js: {
-        dest: 'temp/bower.js',
-        exclude: [
-          'normalize-css',
-          'pocketgrid'
-        ]
-      }
-    },
-    concat_sourcemap: {
-      options: {
-        sourceRoot: '../'
-      },
-      js: {
-        src: ['temp/bower.js',
-              'script/Sintax.js',
-              'script/random-x.js',
-              'script/demo.js',
-              'script/index.js',
-              /*, 'bower_components/eventi/dist/eventi.debug.min.js'*/],
-        dest: 'script/<%= pkg.name %>.js'
-      },
-      css: {
-        src: [
-          'bower_components/normalize-css/normalize.css',
-          'bower_components/pocketgrid/pocketgrid.css',
-          'style/*.css'
-        ],
-        dest: 'style/<%= pkg.name %>.css'
-      }
-    },
-    autoprefixer: {
-      options: {
-        map: true
-      },
-      css: {
-        src: "<%= concat_sourcemap.css.dest %>",
-        dest: "<%= concat_sourcemap.css.dest %>"
+        options: {
+          frame: 'src/plugin-frame.js',
+        },
+        src: ['src/demo-x.js'],
+        dest: 'dist/<%= pkg.name %>.js'
       },
     },
     uglify: {
       options: {
-        sourceMap: true,
-        sourceMapIn: '<%= concat_sourcemap.js.dest %>.map'
+        banner: '<%= banner %>',
+        report: 'gzip'
       },
-      js: {
-        src: '<%= concat_sourcemap.js.dest %>',
-        dest: '<%= concat_sourcemap.js.dest %>'
-      }
+      dist: {
+        src: ['dist/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      },
     },
-    csswring: {
+    compress: {
       options: {
-        map: true
+        mode: 'gzip'
       },
-      css: {
-        src: '<%= autoprefixer.css.dest %>',
-        dest: '<%= autoprefixer.css.dest %>'
-      }
+      dist: {
+        src: ['dist/<%= pkg.name %>.min.js'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      },
     },
-    bake: {
-      html: {
-        files: {
-          "index.html": "html/index.html",
-          "404.html": "html/404.html"
-        }
-      }
+    qunit: {
+      files: ['test/**/*.html']
     },
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      }
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        src: ['src/**/*.js',
+              '!src/*frame.js']
+      },
+      dist: {
+        options: {
+          jshintrc: 'src/.jshintrc-dist'
+        },
+        src: ['dist/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/**/*.js']
+      },
     },
     watch: {
-      js: {
-        files: ['script/demo.js',
-                'script/random-x.js',
-                'script/Sintax.js',
-                'script/index.js',
-                'style/style.css',
-                'html/*.ht*',
-                'bower.json'],
-        tasks: ['build']
-      }
-    }
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src', 'qunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      },
+    },
   });
 
   // Default task.
-  grunt.registerTask('build', ['clean:artifacts', 'bake', 'jshint', 'bower_concat', 'copy', 'concat_sourcemap', 'autoprefixer', 'clean:temp']);
-  grunt.registerTask('minify', ['uglify', 'csswring']);
-  grunt.registerTask('default', ['build', 'minify']);
+  grunt.registerTask('default', ['clean', 'copy', 'jshint', 'uglify', 'compress', 'qunit']);
 
 };
--- script/index.js
+++
@@ -1,1 +0,0 @@
-window.D = document;
